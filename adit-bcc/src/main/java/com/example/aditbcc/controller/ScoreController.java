951baa82e475b0b4f469f3eaab18b28acaebe450
User <aegisorca@gmail.com>
2022-10-05 11:06:58 +0700

    Penambahan Controller Score

---
+++ adit-bcc/src/main/java/com/example/aditbcc/controller/ScoreController.java
@@ -0,0 +1,24 @@
+package com.example.aditbcc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aditbcc.model.Score;
+import com.example.aditbcc.service.ScoreService;
+
+@RestController
+@RequestMapping("/api/score")
+public class ScoreController {
+
+	@Autowired
+	ScoreService scoreService;
+	
+	@GetMapping
+	public List<Score>getAllScore(){
+		return scoreService.getAllScore();
+	}
+}
